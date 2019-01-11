2c769149d189e486afc8ec7516d56091e3228c87
keitakn <keita.koga.work@gmail.com>
2019-01-11 18:59:41 +0900

    :sparkles: #2 Fix to be output with terraform_remote_state

--- src/createS3Backend.ts
+++ src/createS3Backend.ts
@@ -4,6 +4,7 @@ import fs from "fs";
 export interface ICreateBackendParams {
   outputPath: string;
   backendParams: IBackendParams;
+  remoteStateList?: IRemoteState[];
 }
 
 interface IBackendParams {
@@ -16,7 +17,15 @@ interface IBackendParams {
   };
 }
 
-const createS3Template = (backendParams: IBackendParams) => {
+interface IRemoteState {
+  name: string;
+  bucket: string;
+  key: string;
+  region: string;
+  profile: string;
+}
+
+const createS3Template = (backendParams: IBackendParams): string => {
   return `terraform {
   required_version = "${backendParams.requiredVersion}"
 
@@ -29,25 +38,53 @@ const createS3Template = (backendParams: IBackendParams) => {
 }`;
 };
 
+const createRemoteState = (remoteState: IRemoteState): string => {
+  return `data "terraform_remote_state" "${remoteState.name}" {
+  backend = "s3"
+
+  config {
+    bucket  = "${remoteState.bucket}"
+    key     = "${remoteState.key}"
+    region  = "${remoteState.region}"
+    profile = "${remoteState.profile}"
+  }
+}`;
+};
+
+const createS3TemplateWithRemoteStateList = (
+  backendParams: IBackendParams,
+  remoteStateList: IRemoteState[]
+): string => {
+  let remoteStateOutput = "";
+  remoteStateList.map((remoteState: IRemoteState) => {
+    remoteStateOutput += `${createRemoteState(remoteState)}\n\n`;
+  });
+
+  return `terraform {
+  required_version = "${backendParams.requiredVersion}"
+
+  backend "s3" {
+    bucket  = "${backendParams.backend.bucket}"
+    key     = "${backendParams.backend.key}"
+    region  = "${backendParams.backend.region}"
+    profile = "${backendParams.backend.profile}"
+  }
+}
+
+${remoteStateOutput}`;
+};
+
 export const createS3Backend = async (
   params: ICreateBackendParams
-): Promise<any> => {
+): Promise<void> => {
   const writeFile = promisify(fs.writeFile);
 
-  const terraform = createS3Template(params.backendParams);
+  const terraform = params.remoteStateList
+    ? createS3TemplateWithRemoteStateList(
+        params.backendParams,
+        params.remoteStateList
+      )
+    : createS3Template(params.backendParams);
 
   await writeFile(`${params.outputPath}backend.tf`, terraform);
-
-  return {
-    terraform: {
-      required_version: `${params.backendParams.requiredVersion}`,
-
-      backend: {
-        bucket: `${params.backendParams.backend.bucket}`,
-        key: `${params.backendParams.backend.key}`,
-        region: `${params.backendParams.backend.region}`,
-        profile: `${params.backendParams.backend.profile}`
-      }
-    }
-  };
 };
--- test/integration/createS3Backend.spec.ts
+++ test/integration/createS3Backend.spec.ts
@@ -33,4 +33,78 @@ describe("createS3Backend.integrationTest", () => {
 
     expect(resultString).toEqual(expected);
   });
+
+  it("should be able to create a backend.tf with remoteStateList", async () => {
+    const params = {
+      outputPath: "./",
+      backendParams: {
+        requiredVersion: "0.11.10",
+        backend: {
+          bucket: "dev-tfstate",
+          key: "network/terraform.tfstate",
+          region: "ap-northeast-1",
+          profile: "nekochans-dev"
+        }
+      },
+      remoteStateList: [
+        {
+          name: "network",
+          bucket: "dev-tfstate",
+          key: "env:/${terraform.env}/network/terraform.tfstate",
+          region: "ap-northeast-1",
+          profile: "nekochans-dev"
+        },
+        {
+          name: "bastion",
+          bucket: "dev-tfstate",
+          key: "env:/${terraform.env}/bastion/terraform.tfstate",
+          region: "ap-northeast-1",
+          profile: "nekochans-dev"
+        }
+      ]
+    };
+
+    const terraform = {
+      env: "${terraform.env}"
+    };
+
+    const expected = `terraform {
+  required_version = "0.11.10"
+
+  backend "s3" {
+    bucket  = "${params.backendParams.backend.bucket}"
+    key     = "${params.backendParams.backend.key}"
+    region  = "${params.backendParams.backend.region}"
+    profile = "${params.backendParams.backend.profile}"
+  }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+
+  config {
+    bucket  = "dev-tfstate"
+    key     = "env:/${terraform.env}/network/terraform.tfstate"
+    region  = "ap-northeast-1"
+    profile = "nekochans-dev"
+  }
+}
+
+data "terraform_remote_state" "bastion" {
+  backend = "s3"
+
+  config {
+    bucket  = "dev-tfstate"
+    key     = "env:/${terraform.env}/bastion/terraform.tfstate"
+    region  = "ap-northeast-1"
+    profile = "nekochans-dev"
+  }
+}\n\n`;
+
+    await createS3Backend(params);
+
+    const resultString = fs.readFileSync("./backend.tf").toString();
+
+    expect(resultString).toEqual(expected);
+  });
 });
